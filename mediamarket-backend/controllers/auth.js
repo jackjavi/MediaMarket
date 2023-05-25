c9b574b6ecd93d8678f8542eec5a2c6f0cf3d530
jackjavi <jackmtembete@gmail.com>
2023-05-25 05:19:47 +0300

    auth modified

--- mediamarket-backend/controllers/auth.js
+++ mediamarket-backend/controllers/auth.js
@@ -26,7 +26,7 @@ const login = async (req, res) => {
   }
   // compare password
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.OK).json({ username: { name: user.name }, token });
 };
 
 module.exports = {
