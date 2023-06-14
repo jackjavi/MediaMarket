402613763261e43072074c9a898cc4a2ada60dc1
jack javi <jackmtembete@gmail.com>
2023-06-14 06:18:49 +0300

    set limit of 50 mb cloudinary upload

--- mediamarket-backend/app.js
+++ mediamarket-backend/app.js
@@ -44,6 +44,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
