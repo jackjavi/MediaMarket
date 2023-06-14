ba4547664a76a80fcb0782b34cef7583e7c37702
jack javi <jackmtembete@gmail.com>
2023-06-14 05:42:23 +0300

    Access-Control-Allow-Origin

--- mediamarket-backend/app.js
+++ mediamarket-backend/app.js
@@ -35,6 +35,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mediamarket.netlify.app, http://localhost:3000"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
